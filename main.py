5459a681b9e847b30e08242f78c779d50e0b3c61
itsshivangi <Shivangishivangi@my.unt.edu>
2025-11-02 12:31:44 -0600

    final commit with all the updataes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -36,8 +36,11 @@ while game_is_on:
 # Detect R paddle misses
     if ball.xcor() > 390:
         ball.reset_position()
+        scoreboard.l_point()
 # Detect L paddle misses
     if ball.xcor() < -390:
         ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
